accfbe422acdcc423f471fa3992f7526cd21ce02
Magnus Norddahl <dpjudas@users.noreply.github.com>
2016-06-06 22:07:56 +0200

    Switch to burst demo

--- BurstDemo/main.cpp
+++ BurstDemo/main.cpp
@@ -7,7 +7,7 @@ using namespace uicore;
 
 int CALLBACK WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 {
-	//Screen::instance()->run([]() { return std::make_shared<DemoScreenController>(); }, "Scene3D Burst Demo");
-	Screen::instance()->run([]() { return std::make_shared<RaytracerScreenController>(); }, "Scene3D Burst Demo");
+	Screen::instance()->run([]() { return std::make_shared<DemoScreenController>(); }, "Scene3D Burst Demo");
+	//Screen::instance()->run([]() { return std::make_shared<RaytracerScreenController>(); }, "Scene3D Burst Demo");
 	return 0;
 }
