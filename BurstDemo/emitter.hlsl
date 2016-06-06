9810b05b6e68dcff4b99c964a8b3f907bc7204ec
Magnus Norddahl <dpjudas@users.noreply.github.com>
2016-06-06 22:50:00 +0200

    Mooaaar particles!

--- BurstDemo/emitter.hlsl
+++ BurstDemo/emitter.hlsl
@@ -27,23 +27,26 @@ void main(uint3 threadId : SV_DispatchThreadID)
 	// Is it time to emit?
 	if (emitters[index].emit_cooldown <= 0 && emitters[index].life > 0)
 	{
-		// Create a particle
-		Particle particle;
-		particle.pos = emitters[index].pos;
-		particle.velocity.x = cos(radians(emitters[index].emit_position * 10));
-		particle.velocity.y = sin(radians(emitters[index].emit_position * 10));
-		particle.velocity.z = 0;
-		particle.velocity *= 20;
-		particle.life = 4;
-		particle.size = 0.5;
-		particle.particle_subarray_start = 0;
-		particle.particle_subarray_size = 0;
-		particle.emit_position = 0;
-		particle.emit_cooldown = 0;
+		for (int i = 0; i < 10; i++)
+		{
+			// Create a particle
+			Particle particle;
+			particle.pos = emitters[index].pos;
+			particle.velocity.x = cos(radians(emitters[index].emit_position * 5));
+			particle.velocity.y = sin(radians(emitters[index].emit_position * 5));
+			particle.velocity.z = 0;
+			particle.velocity *= 20;
+			particle.life = 10;
+			particle.size = 0.2;
+			particle.particle_subarray_start = 0;
+			particle.particle_subarray_size = 0;
+			particle.emit_position = 0;
+			particle.emit_cooldown = 0;
 
-		// Insert it into particles array:
-		particles[emitters[index].particle_subarray_start + emitters[index].emit_position] = particle;
-		emitters[index].emit_position = (emitters[index].emit_position + 1) % emitters[index].particle_subarray_size;
-		emitters[index].emit_cooldown = 0.005;
+			// Insert it into particles array:
+			particles[emitters[index].particle_subarray_start + emitters[index].emit_position] = particle;
+			emitters[index].emit_position = (emitters[index].emit_position + 1) % emitters[index].particle_subarray_size;
+			emitters[index].emit_cooldown = 0.001;
+		}
 	}
 }
