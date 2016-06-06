726cf3cbc0000d3db3ae97af512dd59886d0ccd7
Kenneth Gangstoe <sphair@github.com>
2016-06-06 22:11:17 +0200

    Particles are now dumb, just moving as they were told to do

--- BurstDemo/particle.hlsl
+++ BurstDemo/particle.hlsl
@@ -21,17 +21,6 @@ void main(uint3 threadId : SV_DispatchThreadID )
 
 	float time_elapsed = 0.016666f;
 
-	if (particles[index].life <= 0)
-	{
-		particles[index].pos = float3(0, 0, 70) + float3(-sin(radians(index)), -cos(radians(index)), -sin(radians(index))) * 4;
-		particles[index].velocity.x = cos(radians(index)) * 2 + index / 400.0;
-		particles[index].velocity.y = sin(radians(index)) * 2 + index / 400.0;
-		particles[index].life = 20;
-		particles[index].size = 1 - index / float(1024) + 0.001;
-	}
-	else
-	{
-		particles[index].pos += particles[index].velocity * time_elapsed;
-		particles[index].life -= time_elapsed;
-	}
+	particles[index].pos += particles[index].velocity * time_elapsed;
+	particles[index].life -= time_elapsed;
 }
